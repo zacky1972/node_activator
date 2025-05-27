a9f5730a4ee7000fe08a866f82da8f9fbaccc9f2
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-28 05:22:55 +0900

    feat: add generate_node_name function to Util module

--- lib/node_activator/util.ex
+++ lib/node_activator/util.ex
@@ -1,2 +1,15 @@
 defmodule NodeActivator.Util do
+  @moduledoc false
+
+  @doc false
+  @spec generate_node_name(binary()) :: {:ok, atom()} | {:error, binary()}
+  def generate_node_name(prefix) do
+    prefix = Regex.replace(~r/\s/, prefix, "-")
+    random_string = :crypto.strong_rand_bytes(5) |> Base.encode32(case: :lower)
+
+    case GetHost.name() do
+      {:ok, hostname} -> {:ok, :"#{prefix}_#{random_string}@#{hostname}"}
+      {:error, reason} -> {:error, reason}
+    end
+  end
 end
--- test/node_activator/util_test.exs
+++ test/node_activator/util_test.exs
@@ -1,4 +1,8 @@
 defmodule NodeActivator.UtilTest do
   use ExUnit.Case
   doctest NodeActivator.Util
+
+  test "succeed generate_node_name/1 always" do
+    assert NodeActivator.Util.generate_node_name("test") |> elem(0) == :ok
+  end
 end
